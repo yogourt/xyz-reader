149c749029c396f51456635025cab9466118332d
Jagoda <jagoda.sylwester@gmail.com>
2018-05-21 14:57:10 +0200

    Some changes in colors and in article list layout

--- XYZReader/src/main/java/com/example/xyzreader/ui/ArticleListActivity.java
+++ XYZReader/src/main/java/com/example/xyzreader/ui/ArticleListActivity.java
@@ -57,11 +57,6 @@ public class ArticleListActivity extends ActionBarActivity implements
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_article_list);
 
-        mToolbar = (Toolbar) findViewById(R.id.toolbar);
-
-
-        final View toolbarContainerView = findViewById(R.id.toolbar_container);
-
         mSwipeRefreshLayout = (SwipeRefreshLayout) findViewById(R.id.swipe_refresh_layout);
 
         mRecyclerView = (RecyclerView) findViewById(R.id.recycler_view);
--- XYZReader/src/main/java/com/example/xyzreader/ui/DynamicHeightNetworkImageView.java
+++ XYZReader/src/main/java/com/example/xyzreader/ui/DynamicHeightNetworkImageView.java
@@ -2,6 +2,7 @@ package com.example.xyzreader.ui;
 
 import android.content.Context;
 import android.util.AttributeSet;
+import android.util.Log;
 
 import com.android.volley.toolbox.NetworkImageView;
 
